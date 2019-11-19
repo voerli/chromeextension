22b294f3bd100790c81200818b0ea782bf2f0fb8
huangshiqi <huangshiqi@fehorizon.com>
2019-11-19 17:01:48 +0800

    新增组件

---
+++ main.js
@@ -0,0 +1,3 @@
+document.getElementById('su').value = '橙子一下'
+document.getElementsByClassName('index-logo-src')[0].src="https://imgsa.baidu.com/forum/w%3D580/sign=380af401a451f3dec3b2b96ca4eff0ec/2e129bef76c6a7ef6b0794edf6faaf51f2de66df.jpg"
+document.getElementsByClassName('qrcode-img')[0].style.background="url(http://5b0988e595225.cdn.sohucs.com/images/20190311/28a3c5af5ce04814af6fa60347edf593.gif) no-repeat";
